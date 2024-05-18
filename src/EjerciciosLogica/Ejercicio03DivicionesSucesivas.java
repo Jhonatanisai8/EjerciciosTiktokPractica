b8273a6c5a7823786e298452e5e0fd59433b3d46
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-17 23:43:07 -0500

    Ejercicio03:Hacer un algoritmo que permita leer dos números N1,N2 y divida N1/N2 sin usar uso del "/"

---
+++ src/EjerciciosLogica/Ejercicio03DivicionesSucesivas.java
@@ -0,0 +1,39 @@
+package EjerciciosLogica;
+
+import java.util.Scanner;
+
+public class Ejercicio03DivicionesSucesivas {
+    static Scanner leer = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        /*
+         * Hacer un algoritmo que permita leer dos números N1,N2 y
+          divida N1/N2 sin usar uso del "/"
+         */
+
+        // definimos las variables
+        int n1, n2, cociente, residuo;
+
+        // pedimos ambos numeros
+        System.out.print("Ingrese el primer número: ");
+        n1 = leer.nextInt();
+        System.out.print("Ingrese el segundo número: ");
+        n2 = leer.nextInt();
+
+        System.out.println(n1 + " / " + n2);
+
+        cociente = 0;
+        residuo = 0;
+
+        while (n1 >= n2) {
+            cociente++;
+            residuo = n1 - n2;
+            n1 = n1 - n2;
+        }
+
+        // imprimimos los resultados
+        System.out.println("Cociente: " + cociente);
+        System.out.println("Residuo: " + residuo);
+
+    }
+}
