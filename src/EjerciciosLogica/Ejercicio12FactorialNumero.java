6b35e87c7efb7960300c7d0de2e4b9100d4925db
jho-my <ojedasnachezjhonatan@gmail.com>
2024-05-21 13:11:33 -0500

    Ejercicio 12: Factorial de un numero hasta que introduzca el numero 0

---
+++ src/EjerciciosLogica/Ejercicio12FactorialNumero.java
@@ -0,0 +1,26 @@
+package EjerciciosLogica;
+
+import java.util.Scanner;
+
+public class Ejercicio12FactorialNumero {
+    static Scanner leer = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        // definimos las variables
+        int numero, factorial;
+        System.out.println("Factorial de un número");
+
+        do {
+            factorial = 1;
+            // pedimos los datos
+            System.out.print("Ingrese el número: ");
+            numero = leer.nextInt();
+            // calculamos el factorial mediante un ciclo
+            for (int i = 1; i <= numero; i++) {
+                factorial *= i;
+            }
+            // mostramos los resultados
+            System.out.println("Factorial de: " + numero + " es: " + factorial);
+        } while (numero != 0);
+    }
+}
