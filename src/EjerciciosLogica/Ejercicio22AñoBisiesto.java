6ca22846359157fdfe51e402480e29b4e6138acf
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-15 21:20:59 -0500

    agregando comnetario al ejercicio 22  sobre si un año es bisiesto o no

--- src/EjerciciosLogica/Ejercicio22AñoBisiesto.java
+++ src/EjerciciosLogica/Ejercicio22AñoBisiesto.java
@@ -1,17 +1,19 @@
 package EjerciciosLogica;
 
 import java.util.Scanner;
-
 public class Ejercicio22AñoBisiesto {
     static Scanner leer = new Scanner(System.in);
 
     public static void main(String[] args) {
         int año;
 
+        //con un ciclo do-while se iran pidiendo los años
         do {
             System.out.print("Ingrese un año: ");
             año = leer.nextInt();
+            //si el año es diferente de 0 se ejecuta la verificacion
             if (año != 0) {
+                //verificamos que el año sea bisiesto 
                 if ((año % 400 == 0) || ((año % 4 == 0) && !(año % 100 == 0))) {
                     System.out.println("El año " + año + " es bisisto");
                 } else {
@@ -21,6 +23,5 @@ public class Ejercicio22AñoBisiesto {
                 System.out.println("Gracias por participar");
             }
         } while (año != 0);
-
     }
 }
