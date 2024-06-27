63de820f12bff614d5dbd520607aff105ee927e5
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-06-26 19:46:33 -0500

    ejercicio 26:   EJERCICIO:   Mostrar las 10 primeras potencias de 3   y la suma de ellos.

---
+++ src/EjerciciosLogica/Ejercicio26Potenciade3.java
@@ -0,0 +1,19 @@
+package EjerciciosLogica;
+
+/*
+  EJERCICIO:
+  Mostrar las 10 primeras potencias de 3
+  y la suma de ellos.
+ */
+public class Ejercicio26Potenciade3 {
+    public static void main(String[] args) {
+        int suma = 0;
+        double potencia;
+        for (int i = 1; i <= 10; i++) {
+            potencia = Math.pow(3, i);
+            System.out.println("3^"+i+" = " +potencia);
+            suma += potencia;
+        }
+        System.out.println("Suma: " + suma);
+    }
+}
