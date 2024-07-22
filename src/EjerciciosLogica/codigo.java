0aef8fcb3282876bcb9781ef6f210984fb22f1bc
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-22 10:37:46 -0500

    class codigo: añadiendo metodos estaticos

---
+++ src/EjerciciosLogica/codigo.java
@@ -0,0 +1,12 @@
+package EjerciciosLogica;
+
+public class codigo {
+
+    public static void println(String mensaje) {
+        System.out.println(mensaje);
+    }
+
+    public static void print(String mensaje) {
+        System.out.print(mensaje);
+    }
+}
