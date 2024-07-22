eff466950841a923905226eb71a890b4824cb1a7
jhonatan <ojedasnachezjhonatan@gmail.com>
2024-07-22 10:31:18 -0500

    EJERCICIO 38  ¿Dado N calcular:(1¹)+(2²)+(3³)+......+(N*N)?

---
+++ src/EjerciciosLogica/Ejercicio33DadoN.java
@@ -0,0 +1,51 @@
+package EjerciciosLogica;
+
+/*EJERCICIO:
+ ¿Dado N calcular:(1¹)+(2²)+(3³)+......+(N*N)?
+*/
+import java.util.Scanner;
+
+public class Ejercicio33DadoN {
+    static Scanner leer = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        /* Definimos variables */
+        int numero, suma = 0;
+
+        /* Pedimos el número */
+        print("Ingrese un número: ");
+        numero = leer.nextInt();
+
+        /* Con un bucle for hacemos las iteraciones */
+        for (int i = 1; i <= numero; i++) {
+            suma += (int) Math.pow(i, i);
+        }
+
+        /* Mostramos el resultado */
+        println("Resultado : " + suma);
+    }
+
+    public static void println(String mensaje) {
+        System.out.println(mensaje);
+    }
+
+    public static void print(String mensaje) {
+        System.out.print(mensaje);
+    }
+}
+
+/*
+ * Algoritmo Ejercicio38DadoN
+ * Definir n,suma Como Entero;
+ * suma = 0;
+ * Escribir "Ingrese un número: ";
+ * leer n;
+ * 
+ * Para i = 1 Hasta n Con Paso 1 Hacer
+ * suma = suma + (i^i);
+ * Fin Para
+ * 
+ * Escribir "El resultado es: ",suma;
+ * FinAlgoritmo
+ */
